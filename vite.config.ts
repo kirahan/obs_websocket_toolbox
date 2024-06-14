6bd2185290cb18ea89a5a47687a5077cd40bd3e0
hanzhao <hanzhao@xiaoman.cn>
2024-06-14 17:21:38 +0800

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/obs_websocket_toolbox/',
   build: {
     // sourcemap: true,
   },
