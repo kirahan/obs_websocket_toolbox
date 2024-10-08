14f62b90022e45df5cb164f31f8f5ca8f2f86334
hanzhao <hanzhao@xiaoman.cn>
2024-10-08 11:10:49 +0800

    port

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import path from 'path'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
-const isDev = process.env.NODE_ENV === 'development'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isDev ? '/' : '/obs_websocket_toolbox/',
+  base: '/obs_websocket_toolbox/',
   build: {
     // sourcemap: true,
   },
